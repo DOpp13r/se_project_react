172be5601b17eab5e828d559d7e0cd07ee924940
Nathan Do <nathando920@gmail.com>
2024-10-27 14:47:41 -0400

    S11 Toggle Switch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../ToggleSwtich/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -15,6 +16,7 @@ function Header({ handleAddClick, weatherData }) {
         {" "}
         {currentDate}, {weatherData.city}
       </p>
+      <ToggleSwitch />
       <button
         className="header__add-button"
         type="button"
